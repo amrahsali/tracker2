70f1251bb793f3b9b14188df9c1f5b94a7866b97
GEORGE <georgequin19@gmail.com>
2023-01-04 22:41:08 +0100

    updated grid

--- app/src/main/java/com/example/tracker/MapFragment.java
+++ app/src/main/java/com/example/tracker/MapFragment.java
@@ -3,6 +3,8 @@ package com.example.tracker;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,6 +17,8 @@ import java.util.ArrayList;
 public class MapFragment extends Fragment  {
 
     GridView coursesGV;
+    private RecyclerView recyclerView;
+    private ArrayList<RecyclerData> recyclerDataArrayList;
 
 
     public MapFragment() {
@@ -28,19 +32,42 @@ public class MapFragment extends Fragment  {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_map, container, false);
 
+//
+//        coursesGV = view.findViewById(R.id.idlocations);
+//        ArrayList<CourseModel> courseModelArrayList = new ArrayList<CourseModel>();
+//
+//        courseModelArrayList.add(new CourseModel("Muhammadu buhari gate", R.drawable.ic_round_school_24));
+//        courseModelArrayList.add(new CourseModel("Fnas", R.drawable.ic_round_school_24));
+//        courseModelArrayList.add(new CourseModel("Famss", R.drawable.ic_round_school_24));
+//        courseModelArrayList.add(new CourseModel("library", R.drawable.ic_round_school_24));
+//        courseModelArrayList.add(new CourseModel("admin complex", R.drawable.ic_round_school_24));
+//        courseModelArrayList.add(new CourseModel("school clinic", R.drawable.ic_round_school_24));
+//
+//        CourseGVAdapter adapter = new CourseGVAdapter(this, courseModelArrayList);
+//        coursesGV.setAdapter(adapter);
 
-        coursesGV = view.findViewById(R.id.idlocations);
-        ArrayList<CourseModel> courseModelArrayList = new ArrayList<CourseModel>();
+        recyclerView = view.findViewById(R.id.idCourseRV);
 
-        courseModelArrayList.add(new CourseModel("Muhammadu buhari gate", R.drawable.ic_round_school_24));
-        courseModelArrayList.add(new CourseModel("Fnas", R.drawable.ic_round_school_24));
-        courseModelArrayList.add(new CourseModel("Famss", R.drawable.ic_round_school_24));
-        courseModelArrayList.add(new CourseModel("library", R.drawable.ic_round_school_24));
-        courseModelArrayList.add(new CourseModel("admin complex", R.drawable.ic_round_school_24));
-        courseModelArrayList.add(new CourseModel("school clinic", R.drawable.ic_round_school_24));
+        // created new array list..
+        recyclerDataArrayList=new ArrayList<>();
 
-        CourseGVAdapter adapter = new CourseGVAdapter(this, courseModelArrayList);
-        coursesGV.setAdapter(adapter);
+        // added data to array list
+        recyclerDataArrayList.add(new RecyclerData("Fnas", R.drawable.ic_round_school_24));
+        recyclerDataArrayList.add(new RecyclerData("Famms", R.drawable.ic_round_school_24));
+        recyclerDataArrayList.add(new RecyclerData("Library", R.drawable.ic_round_school_24));
+        recyclerDataArrayList.add(new RecyclerData("admin complex", R.drawable.ic_round_school_24));
+        recyclerDataArrayList.add(new RecyclerData("clinic", R.drawable.ic_round_school_24));
+
+        // added data from arraylist to adapter class.
+        RecyclerViewAdapter adapter=new RecyclerViewAdapter(recyclerDataArrayList,getActivity());
+
+        // setting grid layout manager to implement grid view.
+        // in this method '2' represents number of columns to be displayed in grid view.
+        GridLayoutManager layoutManager=new GridLayoutManager(getActivity(),2);
+
+        // at last set adapter to recycler view.
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapter);
 
 
         return view;
